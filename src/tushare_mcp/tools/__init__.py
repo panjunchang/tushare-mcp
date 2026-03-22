5f98e133e14a1242bdeec345d9e63c602816dc8a
潘军昌 <jchpan@126.com>
2026-03-22 11:54:35 +0800

    Add register_bar_tools to __all__ exports

--- src/tushare_mcp/tools/__init__.py
+++ src/tushare_mcp/tools/__init__.py
@@ -1,5 +1,10 @@
 # Tools package (bootstrap).
 from .generic import register_generic_tools
 from .export_tools import register_export_tools
+from .bar_tools import register_bar_tools
 
-__all__ = ["register_generic_tools", "register_export_tools"]
+__all__ = [
+    "register_generic_tools",
+    "register_export_tools",
+    "register_bar_tools",
+]
