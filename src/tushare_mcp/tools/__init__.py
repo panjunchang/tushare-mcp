7308801bbfdaa61ddcda93d53512fba5b93983f1
潘军昌 <jchpan@126.com>
2026-03-22 11:56:26 +0800

    Add register_time_tools to tools package

--- src/tushare_mcp/tools/__init__.py
+++ src/tushare_mcp/tools/__init__.py
@@ -2,9 +2,11 @@
 from .generic import register_generic_tools
 from .export_tools import register_export_tools
 from .bar_tools import register_bar_tools
+from .time_tools import register_time_tools
 
 __all__ = [
     "register_generic_tools",
     "register_export_tools",
     "register_bar_tools",
+    "register_time_tools",
 ]
